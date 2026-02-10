7573c6edb49b3065e3ecb6c9c58f5e2bb713643e
Meldak <meldak.z@gmail.com>
2026-02-09 21:14:24 -0600

    Initial Commit

---
+++ Source/Tankanon/TankanonGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankanonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKANON_API ATankanonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
